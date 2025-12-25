9b91bdf42f8d60a5c626b23e03a53ea662b3d487
Chao Ming Huang <mingqed@gmail.com>
2025-12-25 14:56:30 +0800

    Initial Commit

---
+++ FriendsListInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendsListInterview
+//
+//  Created by 黃昭銘 on 2025/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

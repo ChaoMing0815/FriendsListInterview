b32d7bcb09670a1f18310ebc3ca3df0176d224e6
Chao Ming Huang <mingqed@gmail.com>
2025-12-25 22:37:57 +0800

    Set up project structure and add scenarios definition

---
+++ FriendsListInterview/AppConfig/Scenario.swift
@@ -0,0 +1,14 @@
+//
+//  Scenario.swift
+//  FriendsListInterview
+//
+//  Created by 黃昭銘 on 2025/12/25.
+//
+
+import Foundation
+
+enum Scenario {
+    case empty
+    case friendsOnly
+    case friendsWithInvites
+}

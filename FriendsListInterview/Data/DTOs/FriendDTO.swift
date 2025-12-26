e97611f3f6f389b9849a7ead65d377a7e2eb33b1
Chao Ming Huang <mingqed@gmail.com>
2025-12-26 23:13:41 +0800

    Add FriendDTO and DTO-to-domain mapping

---
+++ FriendsListInterview/Data/DTOs/FriendDTO.swift
@@ -0,0 +1,40 @@
+//
+//  FriendDTO.swift
+//  FriendsListInterview
+//
+//  Created by 黃昭銘 on 2025/12/26.
+//
+
+import Foundation
+
+struct FriendDTO: Codable {
+    let name: String
+    let status: Int
+    let isTop: String
+    let fid: String
+    let updateDate: String
+}
+
+// MARK: - DTO to Domain
+extension FriendDTO {
+    func toDomain() -> Friend {
+        Friend(
+            name: name,
+            status: FriendStatus(rawValue: status) ?? .accepted,
+            isTop: isTop == "1" ,
+            fid: fid,
+            updateDate: Self.dateFormatter.date(from: updateDate) ?? .distantPast
+        )
+    }
+}
+
+// MARK: - Parsing Date
+private extension FriendDTO {
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+}
